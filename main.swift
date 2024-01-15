b7181c94507a440f85a59d4f23bfbc1ed380a931
Ole Begemann <ole@oleb.net>
2024-01-15 15:33:40 +0100

    We can use an @main struct as the entry point

--- main.swift
+++ main.swift
@@ -29,35 +29,37 @@ func runtimeInit() {
     ))
 }
 
-@_cdecl("main")
-func main() -> CInt {
-    let onboardLED = 25
-    gpioInit(pin: onboardLED)
-    gpioSetDirection(pin: onboardLED, out: true)
-    gpioSet(pin: onboardLED, high: false)
-
-    let externalLED = 17
-    gpioInit(pin: externalLED)
-    gpioSetDirection(pin: externalLED, out: true)
-    gpioSet(pin: externalLED, high: false)
-
-    let button = 16
-    gpioInit(pin: button)
-    gpioSetDirection(pin: button, out: false)
-    gpioPullUp(pin: button)
-
-    while true {
-        // Blink
-        gpioSet(pin: onboardLED, high: true)
-        delayByCounting(to: 40_000)
-
-        // LED follows button press
-        gpioSet(pin: externalLED, high: !gpioGet(pin: button))
-
+@main
+struct App {
+    static func main() {
+        let onboardLED = 25
+        gpioInit(pin: onboardLED)
+        gpioSetDirection(pin: onboardLED, out: true)
         gpioSet(pin: onboardLED, high: false)
-        delayByCounting(to: 40_000)
 
-        gpioSet(pin: externalLED, high: !gpioGet(pin: button))
+        let externalLED = 17
+        gpioInit(pin: externalLED)
+        gpioSetDirection(pin: externalLED, out: true)
+        gpioSet(pin: externalLED, high: false)
+
+        let button = 16
+        gpioInit(pin: button)
+        gpioSetDirection(pin: button, out: false)
+        gpioPullUp(pin: button)
+
+        while true {
+            // Blink
+            gpioSet(pin: onboardLED, high: true)
+            delayByCounting(to: 40_000)
+
+            // LED follows button press
+            gpioSet(pin: externalLED, high: !gpioGet(pin: button))
+
+            gpioSet(pin: onboardLED, high: false)
+            delayByCounting(to: 40_000)
+
+            gpioSet(pin: externalLED, high: !gpioGet(pin: button))
+        }
     }
 }
 
