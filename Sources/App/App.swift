bd742ccbf9fc3e6666ca594ec2761d36cd33cbcc
Ole Begemann <ole@oleb.net>
2024-02-22 20:59:27 +0100

    Revert Timer API usage for now
    We aren't initializing the RP2040 clocks yet, so the timer doesn't work.

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -10,9 +10,9 @@ struct App {
 
         while true {
             gpioSet(pin: onboardLED, high: true)
-            sleep(milliseconds: 100)
+            delayByCounting(to: 120_000)
             gpioSet(pin: onboardLED, high: false)
-            sleep(milliseconds: 200)
+            delayByCounting(to: 120_000)
         }
     }
 }
@@ -27,4 +27,21 @@ func _exit(_ status: CInt) {
     while true {
       // Infinite loop
     }
+}
+
+/// Artificial delay by counting in a loop.
+///
+/// We can delete this once we can talk to the timer peripheral.
+func delayByCounting(to limit: Int32) {
+    // Optimization barrier
+    @inline(never)
+    func increment(_ value: inout Int32) {
+        value &+= 1
+    }
+    
+    var counter: Int32 = 0
+    while counter < limit {
+        increment(&counter)
+    }
+    return
 }
